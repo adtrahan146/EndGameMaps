28b27c3f4dd803a5c5683382de1cf2cabbcc2653
adtrahan146 <adtrahan@uno.edu>
2022-05-10 16:09:27 -0500

    reformatted a bit before I go off to dinner and such

--- Client/model/client.js
+++ Client/model/client.js
@@ -1,6 +1,9 @@
-
-function clientInit(){
-    getHomepage();
+async function getHomepage(){
+        const view = document.getElementById('client-view');
+        const url = `http://localhost:3000/api/test`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
 }
 
-clientInit();
+window.onload = getHomepage();
--- Client/views/client-views.js
+++ Client/views/client-views.js
@@ -1,12 +1,14 @@
 // const headers = {   'Access-Control-Allow-Origin':'*',
 //                     'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'};
 
-async function getHomepage(){
-    const view = document.getElementById('client-view');
-    const url = `http://localhost:3000/`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    view.innerHTML = data.body;
-}
+// async function getHomepage(){
+//     const view = document.getElementById('client-view');
+//     const url = `http://localhost:3000/`;
+//     const response = await fetch(url);
+
+//     const data = await response.json();
+//     console.log(data);
+
+//     view.innerHTML = data.body;
+// }
 
